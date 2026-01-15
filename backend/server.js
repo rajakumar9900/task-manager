5e304d9707d66d281c10ee9c492a16c5f49b6bd3
manikanta <rajakumar990049@gmail.com>
2026-01-15 19:27:38 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
-
+  //  hi ia mnmn
 const app = express();
 app.use(cors());
 app.use(express.json());
